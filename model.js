91f26c04161047ca6168b2a20c06cc1e13aa0307
Manjinderfi <manjinder.NA@metropilia.fi>
2024-04-01 00:37:38 +0530

    Add message here

---
+++ model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    sender: { type: String, required: true },
+    recipient: { type: String, required: true },
+    content: { type: String, required: true }
+  });
+
+  module.exports = mongoose.model('Message', messageSchema );
+
+
+  c
